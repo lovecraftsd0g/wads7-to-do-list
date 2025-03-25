c098528fdba2a27b1d9590e770f18631480f20f0
the_str4ng3m4n <patrickagastya1403@gmail.com>
2025-03-25 10:12:13 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import todoRoute from "./Routes/todoRoute.js";
+
+const app = express();
+dotenv.config()
+
+app.use(express.json()); // Built-in body-parser for parsing JSON
+app.use(cors()); // Enable Cross-Origin Resource Sharing
+app.use(cookieParser()); // Enable cookie parsing
+
+
+app.use("/service/todo", todoRoute)
+
+// Start the server
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT;
+mongoose.set("strictQuery", true)
+
+// Start the server
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
